6070fdd1fae73c26625cf80e8d184b5d808670e2
Pedro Escoll <pescoll@pasteur.fr>
2023-05-19 08:33:10 +0200

    viewer update
    trying to solve the plotting issue, cells coming back in time. Using a reduced dataset of EP128 to identify cells. At least is plotted as Dmitry cleaned dataset

--- static/js/viewer.js
+++ static/js/viewer.js
@@ -1,19 +1,51 @@
 var currentDatasetFilename = localStorage.getItem('currentDatasetFilename');
+var conditionsDropdown = document.getElementById('conditionsDropdown');
+var parametersDropdown = document.getElementById('parametersDropdown');
+
+axios.get('/get_parameter_names')
+    .then((response) => {
+        response.data.condition_cols.forEach((col) => {
+            var option = document.createElement('option');
+            option.text = col;
+            conditionsDropdown.add(option);
+        });
+        response.data.column_names.forEach((col) => {
+            var option = document.createElement('option');
+            option.text = col;
+            parametersDropdown.add(option);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
 
 function showPlot2() {
-    // Hide the button
-    document.getElementById('showPlot2Button').style.display = 'none';
+    // Display loading message
+    document.getElementById('loadingMessage').style.display = 'block';
 
-    // Make a GET request to the /plot2 endpoint
-    axios.get('/plot2')
-        .then((response) => {
-            // Update the plot image
-            document.getElementById('plotImage2').src = response.data.plot_url;
+    var conditionsDropdown = document.getElementById('conditionsDropdown');
+    var parametersDropdown = document.getElementById('parametersDropdown');
+    
+    var percentageInput = document.getElementById('percentageInput').value;
 
-            // Show the plot area
-            document.getElementById('plotArea2').style.display = 'block';
-        })
-        .catch((error) => {
-            console.log(error);
+    var selectedCondition = conditionsDropdown.options[conditionsDropdown.selectedIndex].value;
+    var selectedParameter = parametersDropdown.options[parametersDropdown.selectedIndex].value;
+
+    axios.post('/plot2', { condition: selectedCondition, parameter: selectedParameter, percentage: percentageInput })
+    .then((response) => {
+        const plotArea2 = document.getElementById('plotArea2');
+        // Clear out the old images
+        plotArea2.innerHTML = '';
+        response.data.plot_urls.forEach(plotUrl => {
+            const img = document.createElement('img');
+            img.src = plotUrl;
+            plotArea2.appendChild(img);
         });
+        plotArea2.style.display = 'block';
+        document.getElementById('loadingMessage').style.display = 'none';
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 }

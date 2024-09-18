e6484fbbbcc767918df767112d9f4adeccdbf8a9
pescoll <pescoll@pasteur.fr>
2024-09-18 09:18:37 +0200

    updated gitignore. Improved UI

--- check_data_types.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def print_column_data_types(filename):
-    df = pd.read_csv(filename)
-    print(df.dtypes)
-
-# Replace with your filename
-print_column_data_types('/Users/pedro/Documents/GitHub/backward_analysis/user_data/cleaned_dataset_EP211_Measurement_1_Evaluation1_Population_-_Nuclei.csv')
